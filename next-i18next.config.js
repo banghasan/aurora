338e270ede3190f2b41a5746f7a7668a55fe3f60
Renato Pozzi <r.pozzi@bluecube.it>
2021-07-10 17:39:40 +0200

    added localePath

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "it"],
   },
+  localePath: path.resolve("./public/static/locales/"),
 };
