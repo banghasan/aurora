63fe8815279b604686299354d4d78f8316dada63
Renato Pozzi <r.pozzi@bluecube.it>
2021-10-30 14:47:24 +0200

    added database icon

---
+++ components/Icons/Database.js
@@ -0,0 +1,14 @@
+export function Database(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      fill="none"
+      viewBox="0 0 48 48"
+      aria-hidden="true"
+      {...props}
+    >
+      <path d="M8 14v20c0 4.418 7.163 8 16 8a31.32 31.32 0 004-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6" />
+    </svg>
+  );
+}
