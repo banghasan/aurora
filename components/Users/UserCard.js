6f18c0799b61b5b0e5c5ec1b728f7a63973e3e53
Renato Pozzi <r.pozzi@bluecube.it>
2021-10-31 14:44:37 +0100

    fix classnames

--- components/Users/UserCard.js
+++ components/Users/UserCard.js
@@ -3,19 +3,19 @@ import { Card } from "../UI/Card";
 export function UserCard(props) {
   return (
     <Card>
-      <div class="relative px-6 py-5 flex items-center space-x-3 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
-        <div class="flex-shrink-0">
+      <div className="relative px-6 py-5 flex items-center space-x-3 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+        <div className="flex-shrink-0">
           <img
-            class="h-10 w-10 rounded-full"
+            className="h-10 w-10 rounded-full"
             src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
             alt=""
           />
         </div>
-        <div class="flex-1 min-w-0">
-          <a href="#" class="focus:outline-none">
-            <span class="absolute inset-0" aria-hidden="true"></span>
-            <p class="text-sm font-medium text-gray-900">{props.name}</p>
-            <p class="text-sm text-gray-500 truncate">{props.role}</p>
+        <div className="flex-1 min-w-0">
+          <a href="#" className="focus:outline-none">
+            <span className="absolute inset-0" aria-hidden="true"></span>
+            <p className="text-sm font-medium text-gray-900">{props.name}</p>
+            <p className="text-sm text-gray-500 truncate">{props.role}</p>
           </a>
         </div>
       </div>
