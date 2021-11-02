c2894a37f469e1baee0ad8bff512911dd4e022c2
Renato Pozzi <r.pozzi@bluecube.it>
2021-11-02 01:29:12 +0100

    added setup routes with tests

--- pages/api/setup.js
+++ pages/api/setup.js
@@ -1,6 +1,7 @@
 import Joi from "joi";
-import db from "../../../lib/db";
-import { formatUser } from "../../../utils/responses";
+import db from "../../lib/db";
+import { formatUser } from "../../utils/responses";
+import prisma from "../../lib/dbInstance";
 
 const schema = Joi.object({
   firstname: Joi.string().required(),
@@ -19,15 +20,31 @@ const schema = Joi.object({
   confirmPassword: Joi.string().valid(Joi.ref("password")).required(),
 });
 
+const checkSetupIsNeeded = async () => {
+  const count = await prisma.user.count();
+  return count < 1;
+};
+
 export default async function handler(req, res) {
   if (req.method === "GET") {
-    const data = await db.getAllUsers();
-    const users = data.map((user) => formatUser(user));
+    const needsSetup = await checkSetupIsNeeded();
 
-    return res.status(200).json(users);
+    if (needsSetup) {
+      return res.json({ message: "Setup not completed" });
+    } else {
+      return res.status(400).json({ message: "Setup already completed" });
+    }
   }
 
   if (req.method === "POST") {
+    const needsSetup = await checkSetupIsNeeded();
+
+    if (!needsSetup) {
+      return res.status(400).json({
+        message: "There is already a user in the database",
+      });
+    }
+
     let value;
 
     try {
---
+++ tests/apis/setup.test.js
@@ -0,0 +1,175 @@
+import { createMocks } from "node-mocks-http";
+import users from "../../mocks/users.json";
+import prisma from "../../lib/dbInstance";
+import handler from "../../pages/api/setup";
+
+describe("POST /setup", () => {
+  beforeAll(async () => {
+    await prisma.$executeRawUnsafe(`TRUNCATE TABLE "users" RESTART IDENTITY`);
+  });
+
+  it("should create the first user", async () => {
+    const { req, res } = createMocks({
+      method: "POST",
+      body: {
+        firstname: "John",
+        lastname: "Doe",
+        email: "john.doe@example.com",
+        password: "password",
+        confirmPassword: "password",
+      },
+    });
+
+    await handler(req, res);
+
+    console.log(res._getJSONData());
+    expect(res._getStatusCode()).toBe(201);
+    expect(res._getJSONData()).toMatchObject({
+      email: "john.doe@example.com",
+      firstname: "John",
+      lastname: "Doe",
+    });
+  });
+
+  it("should return an error if the passwords don't match", async () => {
+    const { req, res } = createMocks({
+      method: "POST",
+      body: {
+        firstname: "John",
+        lastname: "Doe",
+        email: "john.doe@example.com",
+        password: "password",
+        confirmPassword: "password2",
+      },
+    });
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(400);
+  });
+
+  it("should return an error if the password is too short", async () => {
+    const { req, res } = createMocks({
+      method: "POST",
+      body: {
+        firstname: "John",
+        lastname: "Doe",
+        email: "john.doe@example.com",
+        password: "pass",
+        confirmPassword: "pass",
+      },
+    });
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(400);
+  });
+
+  it("should return an error if the firstname is missing", async () => {
+    const { req, res } = createMocks({
+      method: "POST",
+      body: {
+        lastname: "Doe",
+        email: "john.doe@example.com",
+        password: "password",
+        confirmPassword: "password",
+      },
+    });
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(400);
+  });
+
+  it("should return an error if the lastname is missing", async () => {
+    const { req, res } = createMocks({
+      method: "POST",
+      body: {
+        firstname: "John",
+        email: "john.doe@example.com",
+        password: "password",
+        confirmPassword: "password",
+      },
+    });
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(400);
+  });
+
+  it("should return an error if the email is missing", async () => {
+    const { req, res } = createMocks({
+      method: "POST",
+      body: {
+        firstname: "John",
+        lastname: "Doe",
+        password: "password",
+        confirmPassword: "password",
+      },
+    });
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(400);
+  });
+
+  it("should return an error if the password is missing", async () => {
+    const { req, res } = createMocks({
+      method: "POST",
+      body: {
+        firstname: "John",
+        lastname: "Doe",
+        email: "john.doe@example.com",
+        confirmPassword: "password",
+      },
+    });
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(400);
+  });
+
+  it("should return an error if the confirmPassword is missing", async () => {
+    const { req, res } = createMocks({
+      method: "POST",
+      body: {
+        firstname: "John",
+        lastname: "Doe",
+        email: "john.doe@example.com",
+        password: "password",
+      },
+    });
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(400);
+  });
+});
+
+describe("GET /setup", () => {
+  beforeAll(async () => {
+    await prisma.$executeRawUnsafe(`TRUNCATE TABLE "users" RESTART IDENTITY`);
+  });
+
+  it("should return 200 because the setup is not done", async () => {
+    const { req, res } = createMocks({
+      method: "GET",
+    });
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(200);
+  });
+
+  it("should return 400 because the setup is already done", async () => {
+    await prisma.user.createMany({ data: users });
+
+    const { req, res } = createMocks({
+      method: "GET",
+    });
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(400);
+  });
+});
