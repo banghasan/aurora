c6362e28160582dceb19c7bc305e48a86f4c0e1f
Renato Pozzi <r.pozzi@bluecube.it>
2021-11-07 00:13:42 +0100

    configured index page

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,11 @@
+import { useMeWebsites } from "../lib/hooks/useMeWebsites";
 import { Container } from "../components/UI/Container";
 import { Heading } from "../components/Heading/Heading";
 import { WebsiteList } from "../components/Websites/WebsiteList";
-import { useAuth } from "../lib/hooks/useAuth";
 
-const websites = [
-  {
-    id: 1,
-    name: "Roodel",
-    url: "http://wunderground.com",
-    is_public: true,
-  },
-  {
-    id: 2,
-    name: "Feedmix",
-    url: "https://canalblog.com",
-    is_public: false,
-  },
-  {
-    id: 3,
-    name: "Innojam",
-    url: "http://ebay.com",
-    is_public: false,
-  },
-  {
-    id: 4,
-    name: "Mybuzz",
-    url: "https://webmd.com",
-    is_public: false,
-  },
-  {
-    id: 5,
-    name: "Zoombeat",
-    url: "https://paypal.com",
-    is_public: true,
-  },
-  {
-    id: 6,
-    name: "Demizz",
-    url: "http://reuters.com",
-    is_public: true,
-  },
-  {
-    id: 7,
-    name: "Nlounge",
-    url: "https://hp.com",
-    is_public: true,
-  },
-];
-
-export default function Home(props) {
-  const { user } = useAuth();
-
-  console.log("user ", user);
+export default function Home() {
+  const { websites, isLoading, isError } = useMeWebsites();
+  const isLoaded = !isLoading && !isError;
 
   return (
     <Container>
@@ -61,7 +15,8 @@ export default function Home(props) {
         nibh mattis, in aliquam dui condimentum.
       </Heading>
 
-      <WebsiteList websites={websites} />
+      {!isLoaded && <p>Loading...</p>}
+      {isLoaded && <WebsiteList websites={websites} />}
     </Container>
   );
 }
