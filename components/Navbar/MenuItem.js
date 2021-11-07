45745462d28322bb2208aa2c74791351292e9ce4
Renato Pozzi <r.pozzi@bluecube.it>
2021-11-07 14:45:41 +0100

    added role button to menuItem

--- components/Navbar/MenuItem.js
+++ components/Navbar/MenuItem.js
@@ -1,11 +1,24 @@
 import Link from "next/link";
 
 export function MenuItem(props) {
+  const classNames =
+    "text-base font-medium px-2 md:px-3 py-2 text-gray-700 hover:text-black focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2";
+
+  if (props.role === "button") {
+    return (
+      <button className={classNames} onClick={props.onClick}>
+        {props.label}
+      </button>
+    );
+  }
+
   return (
     <Link href={props.href}>
-      <a className="text-base font-medium px-2 md:px-3 py-2 text-gray-700 hover:text-black focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2">
-        {props.label}
-      </a>
+      <a className={classNames}>{props.label}</a>
     </Link>
   );
 }
+
+MenuItem.defaultProps = {
+  role: "link",
+};
