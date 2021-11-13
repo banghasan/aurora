19003e54ffd5e19474c4d17239fc59601bda5521
Renato Pozzi <r.pozzi@bluecube.it>
2021-11-13 17:41:59 +0100

    datatable subcomponents

--- components/Datatable/Row.js
+++ components/Datatable/Row.js
@@ -1,15 +1,15 @@
 export function Row(props) {
   return (
-    <div class="relative py-2">
+    <div className="relative py-2">
       <div
-        class="absolute inset-0 h-full rounded bg-gray-100"
+        className="absolute inset-0 h-full rounded bg-gray-100"
         style={{ width: `${props.percentage}%` }}
       />
 
-      <div class="grid grid-cols-3 gap-4 text-sm px-3 relative">
+      <div className="grid grid-cols-3 gap-4 text-sm px-3 relative">
         <dd className="truncate">{props.element}</dd>
-        <dd class="text-right font-medium">{props.views}</dd>
-        <dd class="text-right font-medium">{props.unique}</dd>
+        <dd className="text-right font-medium">{props.views}</dd>
+        <dd className="text-right font-medium">{props.unique}</dd>
       </div>
     </div>
   );
--- components/Datatable/Thead.js
+++ components/Datatable/Thead.js
@@ -1,9 +1,9 @@
 export function Thead() {
   return (
-    <div class="grid grid-cols-3 gap-4 text-sm font-medium text-gray-500 pb-2">
+    <div className="grid grid-cols-3 gap-4 text-sm font-medium text-gray-500 pb-2">
       <dt>Content</dt>
-      <dt class="text-right">Views</dt>
-      <dt class="text-right">Unique</dt>
+      <dt className="text-right">Views</dt>
+      <dt className="text-right">Unique</dt>
     </div>
   );
 }
