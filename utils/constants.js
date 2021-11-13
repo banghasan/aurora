6dc84dea1f2fc54f66e586d22ed09192839454c9
Renato Pozzi <r.pozzi@bluecube.it>
2021-11-13 17:40:26 +0100

    added colors in constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,11 @@
 export const AUTH_COOKIE = "aurorasession";
 export const AUTH_COOKIE_LIFETIME = 60 * 60 * 24 * 1;
+export const COLORS = [
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
+];
