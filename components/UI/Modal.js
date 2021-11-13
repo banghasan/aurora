935fcb332bc1d6b97977f175a86535c7c7b485cd
Renato Pozzi <r.pozzi@bluecube.it>
2021-11-13 17:41:12 +0100

    modfal improvements

--- components/UI/Modal.js
+++ components/UI/Modal.js
@@ -1,29 +1,27 @@
 import ReactDOM from "react-dom";
+import { useState, useEffect } from "react";
 import { Card } from "./Card";
-import { Button } from "./Button";
-import { SubTitle } from "./SubTitle";
 
 export function Modal(props) {
-  const { isOpen, onClose } = props;
+  const { isOpen } = props;
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   if (!isOpen) {
     return null;
   }
 
   const el = (
-    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity flex items-center justify-center">
-      <Card className="max-w-lg bg-white p-6">
-        <div className="space-y-3 mb-10 items-center flex flex-col">
-          <SubTitle>Wait a second!</SubTitle>
-          <div className="max-w-sm flex justify-center text-center text-lg text-gray-600">
-            It seems this is you're first time here, let's create your first
-            user!
-          </div>
-        </div>
-        <Button onClick={onClose} label="Let's Go!" />
-      </Card>
+    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity flex items-center justify-center overflow-y-hidden h-screen">
+      <Card className="max-w-lg bg-white p-6">{props.children}</Card>
     </div>
   );
 
-  return ReactDOM.createPortal(el, document.getElementById("modal-portal"));
+  return (
+    isMounted &&
+    ReactDOM.createPortal(el, document.getElementById("modal-portal"))
+  );
 }
