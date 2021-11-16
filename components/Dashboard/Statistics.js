101b116834fc9ee7a6eb85fcd1a70fb99aef61c8
Renato Pozzi <r.pozzi@bluecube.it>
2021-11-16 19:29:29 +0100

    added animations & formats

--- components/Dashboard/Statistics.js
+++ components/Dashboard/Statistics.js
@@ -1,13 +1,36 @@
+import { animate } from "framer-motion";
+import { useEffect, useRef } from "react";
+import { formatter } from "../../utils/math";
 import { Card } from "../UI/Card";
 
 export function Statistic(props) {
+  const numberRef = useRef();
+
+  useEffect(() => {
+    const node = numberRef.current;
+
+    const controls = animate(0, props.value, {
+      delay: 0.2,
+      type: "spring",
+      stiffness: 10,
+      onUpdate: (v) => {
+        node.textContent = formatter(Math.round(v));
+      },
+    });
+
+    return () => controls.stop();
+  }, [props.value]);
+
   return (
     <div>
-      <div className="tracking-tighter text-md text-gray-500">
+      <div className="tracking-tighter text-lg text-gray-500">
         {props.label}
       </div>
-      <div className="font-bold tracking-tighter text-3xl text-gray-900">
-        {props.value}
+      <div
+        className="font-bold tracking-tighter text-5xl text-gray-900"
+        ref={numberRef}
+      >
+        0
       </div>
     </div>
   );
@@ -18,11 +41,11 @@ export function Statistics(props) {
     <Card className="p-6">
       <div className="flex flex-col space-y-6">
         <div className="flex space-x-14">
-          <Statistic label="Page Views" value="40k" />
-          <Statistic label="Unique Visits" value="20k" />
-          <Statistic label="Bounces" value="354" />
-          <Statistic label="Visit Duration" value="45s" />
-          <Statistic label="Current Visitors" value="12" />
+          <Statistic label="Page Views" value={4000000000} />
+          <Statistic label="Unique Visits" value={200000000} />
+          <Statistic label="Bounces" value={354325} />
+          <Statistic label="Visit Duration" value={45} />
+          <Statistic label="Current Visitors" value={12} />
         </div>
       </div>
     </Card>
--- utils/math.js
+++ utils/math.js
@@ -1,5 +1,23 @@
-const percentage = (n, t) => (n / t) * 100;
+export const percentage = (n, t) => (n / t) * 100;
 
-const sum = (args = []) => args.reduce((acc, el) => acc + el, 0);
+export const sum = (args = []) => args.reduce((acc, el) => acc + el, 0);
 
-module.exports = { percentage, sum };
+export const formatter = (number) => {
+  if (typeof number === "string") {
+    number = Number(number);
+  }
+
+  if (number < 1000) {
+    return number;
+  }
+
+  if (number < 1000000) {
+    return `${(number / 1000).toFixed(1)}k`;
+  }
+
+  if (number < 1000000000) {
+    return `${(number / 1000000).toFixed(1)}M`;
+  }
+
+  return `${(number / 1000000000).toFixed(1)}B`;
+};
